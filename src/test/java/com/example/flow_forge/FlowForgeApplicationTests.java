2fc6d86156ba3b1974ce770dd96da0825c5c66d7
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-16 16:37:44 +0300

    Initial commit

---
+++ src/test/java/com/example/flow_forge/FlowForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flow_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
