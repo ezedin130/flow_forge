74df9dd30b059d4025a5aec3fe4047c602b39765
Ezedin Awol <140709867+ezedin130@users.noreply.github.com>
2025-12-22 10:15:20 +0300

    feat: entity class for user

---
+++ src/main/java/com/example/flow_forge/model/User.java
@@ -0,0 +1,45 @@
+package com.example.flow_forge.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "first name is required")
+    private String firstName;
+    @NotBlank(message = "last name is required")
+    private String lastName;
+    @NotBlank(message = "user name is required")
+    private String userName;
+    @NotBlank(message = "email is required")
+    private String email;
+    @NotBlank(message = "password is required")
+    private String password;
+    @NotNull(message = "is active is required")
+    private Boolean isActive;
+    @NotNull(message = "created At is required")
+    private LocalDateTime createdAt;
+    @NotNull(message = "tenant is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tenant_id")
+    private Tenant tenant;
+    //todo: the relationship between user and role should be implemented once role entity is implemented
+    //@NotNull(message = "role is required")
+    //    @ManyToMany(fetch = FetchType.LAZY)
+    //    @JoinColumn(name = "role_id")
+    //    private Role role;
+}
